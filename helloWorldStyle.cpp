ad922b2aabc2afd4a4ed69661bc9d8117f6b1538
Ubuntu <ubuntu@ip-172-31-52-96.ec2.internal>
2022-02-18 03:17:40 +0000

    created code and finished file

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Mr. Riscalas All rights reserved
+//
+// Created By: Nicolas Riscalas
+// Created On February 17 2022
+// Hello world with proper style
+
+
+#include <iostream>
+using std::cout;
+
+int main() {
+    cout << "Hello, World!\n";
+}
